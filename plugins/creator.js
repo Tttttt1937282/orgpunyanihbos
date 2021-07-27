5790f9dc7200c2d11ad3afcbb1a3d31f5b9ec149
BotPunya-ORG <88033612+BotPunya-ORG@users.noreply.github.com>
2021-07-27 18:09:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.fakeReply(m.chat, 'This is my owner', '6281390658325@s.whatsapp.net', 'Owner')
+  this.fakeReply(m.chat, 'This is my owner', '0@s.whatsapp.net', 'Owner')
   this.sendContact(m.chat, '6281949589655', 'ORG', m)
 }
 
