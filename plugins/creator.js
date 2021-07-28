76a6e8bba71a21428f2fb6dfab587693ae3a81e5
Tttttt1937282 <87937449+Tttttt1937282@users.noreply.github.com>
2021-07-28 20:08:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   this.sendContact(m.chat, '6281949589655', 'ORG', m)
-    this.fakeReply(m.chat, 'itu owner ku jangan di spam atau ku blok kamu👍', '0@s.whatsapp.net', 'Owner')
+    this.fakeReply(m.chat, 'itu owner ku jangan di spam atau ku blok kamu🤗', '0@s.whatsapp.net', 'Owner')
 }
 
 handler.help = ['owner', 'creator']
