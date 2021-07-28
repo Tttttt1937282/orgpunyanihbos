4716fb0c01678bca7a1a2ac7d35fcf76eb28493c
BotPunya-ORG <88033612+BotPunya-ORG@users.noreply.github.com>
2021-07-28 11:57:47 +0700

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,8 +1,9 @@
+  
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 4999
-let handler  = async (m, { conn, usedPrefix }) => {
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (id in conn.caklontong) {
@@ -10,19 +11,25 @@ let handler  = async (m, { conn, usedPrefix }) => {
         throw false
     }
     let res = await fetch(global.API('bg',  '/caklontong'))
+    if (res.status !== 200) throw await res.text()
     let json = await res.json()
-    if (json.status !== true) return m.reply(json)
+    let caption = `
+${json.result.soal}
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
+`.trim()
     conn.caklontong[id] = [
-      await conn.reply(m.chat, `Soal: *${json.result.soal}*\nTimeout: *${(timeout / 1000).toFixed(2)} detik*\nKetik *${usedPrefix}hint caklontong* untuk hint\nBonus: ${poin} XP`, m),
-      json, poin,
-      setTimeout(() => {
-        if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\n*${json.result.jawaban}${json.result.deskripsi ? ' (' + json.result.deskripsi + ')' : ''}*`, conn.caklontong[id][0])
-        delete conn.caklontong[id]
-      }, timeout)
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.caklontong[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*\n${json.result.deskripsi}`, conn.caklontong[id][0])
+            delete conn.caklontong[id]
+        }, timeout)
     ]
-  }
-  handler.help = ['caklontong']
-  handler.tags = ['game']
-  handler.command = /^caklontong/i
-  
-  module.exports = handler
+}
+handler.help = ['caklontong']
+handler.tags = ['game']
+handler.command = /^caklontong/i
+
+module.exports = handler
