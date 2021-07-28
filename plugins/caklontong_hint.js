164e258857012819ae713c6b3017ad52b1389de3
BotPunya-ORG <88033612+BotPunya-ORG@users.noreply.github.com>
2021-07-28 12:06:57 +0700

    Update caklontong_hint.js

--- plugins/caklontong_hint.js
+++ plugins/caklontong_hint.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
     if (!(id in conn.caklontong)) throw false
     let json = conn.caklontong[id][1]
     let ans = json.result.jawaban
-    let clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
+    let clue = ans.replace(/[]/g, '_')
     m.reply('```' + clue + '```')
 }
 handler.command = /^calo$/i
